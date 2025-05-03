4ccc0ad988bfcce367acc332dabde76535392221
TadeoPariani <tadeopariani1@gmail.com>
2025-05-02 21:03:28 -0300

    ?

--- Back/financeApp/middleware.py
+++ Back/financeApp/middleware.py
@@ -4,6 +4,6 @@ from django.urls import resolve
 class DisableCSRFOnRegisterAndLoginMiddleware(MiddlewareMixin):
     def process_view(self, request, callback, callback_args, callback_kwargs): #Hace que el Token no sea requerio en estos endpoints
         resolved_path = resolve(request.path_info).route  # Obtiene la ruta sin el prefijo de la app
-        if resolved_path in ['auth/login/', 'auth/register/'] and request.method == 'POST':
+        if resolved_path in ['auth/login/', 'auth/register/'] and (request.method == 'POST' or request.method == 'GET'):
             setattr(request, '_dont_enforce_csrf_checks', True)
         return None
--- Back/financeBack/settings.py
+++ Back/financeBack/settings.py
@@ -32,18 +32,21 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
     'rest_framework',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'financeApp',
-    'corsheaders',
+    'rest_framework.authtoken',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django_filters",
+    "financeApp",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,7 +54,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 CORS_ALLOW_CREDENTIALS = True
